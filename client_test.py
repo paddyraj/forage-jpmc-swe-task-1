d485590f6215a13c3b7ba363f13c6dfc800891da
paddyraj <padgaonkarr@gmail.com>
2023-06-21 13:46:18 -0400

    Add files via upload

--- client_test.py
+++ client_test.py
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for q in quotes:
+      self.assertEqual(getDataPoint(q),(q['stock'],q['top_bid']['price'],q['top_ask']['price'],(q['top_bid']['price']+q['top_ask']['price'])/2))
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +18,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for q in quotes:
+      self.assertGreater(getDataPoint(q),(q['stock'],q['top_bid']['price']))
 
   """ ------------ Add more unit tests ------------ """
-
 
 
 if __name__ == '__main__':
